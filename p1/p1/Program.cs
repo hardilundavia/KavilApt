7eb8aab2b3d187c639267a1d224bb1858ca2c13f
hardilundavia <50069843+hardilundavia@users.noreply.github.com>
2019-12-18 10:16:19 +0530

    Add files via upload

---
+++ p1/p1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many Number Do you want to enter : ");
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            String[] s1 = new string[n];
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine("Enter A String : ",i);
+                s1[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("String is : {0} ",s1[i]);
+            }
+            int count = 0;
+            for(int i=0;i<n;i++)
+            {
+                for(int j=i+1;j<n;j++)
+                {
+                    if (s1[i] == s1[j])
+                        count++;
+                }
+            }
+            Console.WriteLine("\nTotal {0} ",count+" is Same.");
+        }
+    }
+}
---
+++ p2/p2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2
+{
+    class Area
+    {
+        double a = new double();
+        const double pi = 3.14;
+        public void area(double r)
+        {
+            a = pi * (r * r);
+            Console.WriteLine("Area of Circle is : {0}  ",a);
+        }
+        public void area(double l,double w)
+        {
+            a = l * w;
+            Console.WriteLine("Area of Ractangle is : {0}  ", a);
+        }
+        public void area(double s,int e)
+        {
+            a = s * s;
+            Console.WriteLine("Area of Square is : {0}  ", a);
+        }
+        public void area(double b,double h, int e)
+        {
+            a =(b * h) / 2;
+            Console.WriteLine("Area of Triangle is : {0}  ", a);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double r,l,w,s,b,h = new double();
+            Area a = new Area();
+            Console.WriteLine("\nArea of Circle \n");
+            Console.WriteLine("Enter The Value of R : ");
+            r = Convert.ToDouble(Console.ReadLine());
+            a.area(r);
+
+            Console.WriteLine("\nArea of Ractangle \n");
+            Console.WriteLine("Enter The Value of L : ");
+            l = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter The Value of W : ");
+            w = Convert.ToDouble(Console.ReadLine());
+            a.area(l, w);
+
+            Console.WriteLine("\nArea of Square \n");
+            Console.WriteLine("Enter The Value of s : ");
+            s = Convert.ToDouble(Console.ReadLine());
+            a.area(s, 0);
+
+            Console.WriteLine("\nArea of Triangle \n");
+            Console.WriteLine("Enter The Value of B : ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter The Value of H : ");
+            h = Convert.ToDouble(Console.ReadLine());
+            a.area(b,h,0);
+        }
+    }
+}
